d3c5de32b8e0fcd937990d3e80d64e4c6c43e7ad
Palani Akash <80441653+palaniakash1@users.noreply.github.com>
2024-02-28 21:37:31 +0530

    first commit

---
+++ Student-datamanagement-backend/studentDatamanagement/src/main/java/org/student/StudentDatamanagementApplication.java
@@ -0,0 +1,13 @@
+package org.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDatamanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDatamanagementApplication.class, args);
+	}
+
+}
---
+++ Student-datamanagement-backend/studentDatamanagement/src/main/java/org/student/entity/Student.java
@@ -0,0 +1,8 @@
+package org.student.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Student {
+
+}
